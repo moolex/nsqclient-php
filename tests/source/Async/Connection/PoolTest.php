6a180abf9d356ea0a2759c0ba6654db0ee8ecf07
moyo <git@moyo.im>
2017-09-04 15:28:00 +0800

    unit tests for Pool update

--- tests/source/Async/Connection/PoolTest.php
+++ tests/source/Async/Connection/PoolTest.php
@@ -18,26 +18,29 @@ class PoolTest extends \PHPUnit_Framework_TestCase
 
     public function test_hosting_read()
     {
+        $classN1 = new class { public function name() { return 'n1'; } };
+        $classN2 = new class { public function name() { return 'n2'; } };
+
         $nodeID_1 = null;
         $nodeID_2 = null;
 
-        $result_1 = Pool::hosting(Pool::MOD_R, $this->node, $this->flags, function ($slotID) use (&$nodeID_1) {
+        $result_1 = Pool::hosting(Pool::MOD_R, $this->node, $this->flags, function ($slotID) use (&$nodeID_1, $classN1) {
             // first initialize
             $nodeID_1 = $slotID;
-            return 'node-1';
+            return new $classN1;
         });
 
         // test if got right node
-        $this->assertEquals('node-1', $result_1);
+        $this->assertEquals('n1', $result_1->name());
 
-        $result_2 = Pool::hosting(Pool::MOD_R, $this->node, $this->flags, function ($slotID) use (&$nodeID_2) {
+        $result_2 = Pool::hosting(Pool::MOD_R, $this->node, $this->flags, function ($slotID) use (&$nodeID_2, $classN2) {
             // never initialize
             $nodeID_2 = $slotID;
-            return 'node-2';
+            return new $classN2;
         });
 
         // test if got new node (force locking for subscribe)
-        $this->assertEquals('node-2', $result_2);
+        $this->assertEquals('n2', $result_2->name());
 
         // test slot id is valid
         $this->assertTrue(is_numeric($nodeID_1));
@@ -46,27 +49,31 @@ class PoolTest extends \PHPUnit_Framework_TestCase
 
     public function test_hosting_write()
     {
+        $classN1 = new class { public function name() { return 'n1'; } };
+        $classN2 = new class { public function name() { return 'n2'; } };
+        $classN3 = new class { public function name() { return 'n3'; } };
+
         $nodeID_1 = null;
         $nodeID_2 = null;
         $nodeID_3 = null;
 
-        $result_1 = Pool::hosting(Pool::MOD_W, $this->node, $this->flags, function ($slotID) use (&$nodeID_1) {
+        $result_1 = Pool::hosting(Pool::MOD_W, $this->node, $this->flags, function ($slotID) use (&$nodeID_1, $classN1) {
             // first initialize
             $nodeID_1 = $slotID;
-            return 'node-1';
+            return new $classN1;
         });
 
         // test if got right node
-        $this->assertEquals('node-1', $result_1);
+        $this->assertEquals('n1', $result_1->name());
 
-        $result_2 = Pool::hosting(Pool::MOD_W, $this->node, $this->flags, function ($slotID) use (&$nodeID_2) {
+        $result_2 = Pool::hosting(Pool::MOD_W, $this->node, $this->flags, function ($slotID) use (&$nodeID_2, $classN2) {
             // second initialize
             $nodeID_2 = $slotID;
-            return 'node-2';
+            return new $classN2;
         });
 
         // test if got diff node
-        $this->assertEquals('node-2', $result_2);
+        $this->assertEquals('n2', $result_2->name());
 
         // test if pool is new
         $this->assertNotEquals($nodeID_1, $nodeID_2);
@@ -74,14 +81,14 @@ class PoolTest extends \PHPUnit_Framework_TestCase
         // unlocking
         call_user_func_array(Pool::lockCallback(function ($r) {}), ['T_Result', $nodeID_2]);
 
-        $result_3 = Pool::hosting(Pool::MOD_W, $this->node, $this->flags, function ($slotID) use (&$nodeID_3) {
+        $result_3 = Pool::hosting(Pool::MOD_W, $this->node, $this->flags, function ($slotID) use (&$nodeID_3, $classN3) {
             // second initialize
             $nodeID_3 = $slotID;
-            return 'node-3';
+            return new $classN3;
         });
 
         // test if got node_2
-        $this->assertEquals('node-2', $result_3);
+        $this->assertEquals('n2', $result_3->name());
 
         // test if pool is reused
         $this->assertEquals(null, $nodeID_3);
@@ -89,6 +96,8 @@ class PoolTest extends \PHPUnit_Framework_TestCase
 
     public function test_release()
     {
+        $classN = new class { public function name() { return 'n'; } };
+
         $cliMax = 100;
 
         $slotIDs = [];
@@ -97,7 +106,10 @@ class PoolTest extends \PHPUnit_Framework_TestCase
 
         for ($i = 0; $i < $cliMax; $i ++)
         {
-            Pool::hosting(Pool::MOD_W, $this->node, $this->flags, function ($slotID) use (&$slotIDs) {$slotIDs[] = $slotID;});
+            Pool::hosting(Pool::MOD_W, $this->node, $this->flags, function ($slotID) use (&$slotIDs, $classN) {
+                $slotIDs[] = $slotID;
+                return new $classN;
+            });
         }
 
         $memory_finish = memory_get_usage();
